2888ed6a81e080a21bfaa2b7209269dd0cd0fde7
Taynara <taynarasantossousa09@gmail.com>
2025-04-14 20:44:43 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+var frutas = ["Banana","Laranja"]
+
+/* .push() adiciona item ao final da lista */
+/* frutas.push("Uva")
+frutas.push("Maça") */
+
+/* .unshift() adiciona o item no inicio da lista */
+/* frutas.unshift("manga")
+ */
+/* .pop() remove o ultimo item da lista */
+/* frutas.pop() */
+
+/* .shift() remove o primeiro item da lista */
+/* frutas.shift() */
+
+/* frutas.splice(1,1) */
